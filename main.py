857127454c29de8146dc71d04bcfc0d0a7013ef0
parnika-5 <97643131+parnika-5@users.noreply.github.com>
2024-07-15 23:06:17 +0000

    Base menu code

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+def menu():
+    print("Welcome to The GUARDIAN INTERACTIVE SECURITY SYSTEM")
 
+
+
+
+    print("Please set up an ADMIN user profile:")
+    username = input("Please enter your username: ")
+    password = input("Please enter your password: ")
+    
+    print(f"\nUsername: {username}")
+    print("Password: " + "*" * len(password))  
+    print("Is this correct?")
+
+    print("Type the number 1 for YES or 2 for NO")
+    correct = input("Enter number: ")
+
+    if correct == '1':
+        print("Perfect!! Welcome to the GISS")
+
+    elif correct == '2':
+        print("Sorry about that! Please enter your information again:")
+        #create admin menu System. 
+
+menu()
