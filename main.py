8e5b88f10c2000670bb2041ba6e7ea8a61ee1ac5
parnika-5 <97643131+parnika-5@users.noreply.github.com>
2024-07-16 20:37:20 +0000

    UPDATED main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-def menu():
+def ADMIN_menu_SETUP():
     print("Welcome to The GUARDIAN INTERACTIVE SECURITY SYSTEM")
 
-
-
-
     print("Please set up an ADMIN user profile:")
-    username = input("Please enter your username: ")
-    password = input("Please enter your password: ")
+    ADMIN_username = input("Please enter your username: ")
+    ADMIN_password = input("Please enter your password: ")
     
-    print(f"\nUsername: {username}")
-    print("Password: " + "*" * len(password))  
+    print(f"\nUsername: {ADMIN_username}")
+    print("Password: " + "*" * len(ADMIN_password))  
     print("Is this correct?")
 
     print("Type the number 1 for YES or 2 for NO")
@@ -22,4 +19,41 @@ def menu():
         print("Sorry about that! Please enter your information again:")
         #create admin menu System. 
 
-menu()
+def ADMIN_menu():
+1. change permissions
+2. change password
+3. CALL OUR HELPLINE
+
+
+
+def main_menu():
+    print("Choose an activity:")
+    print("1. Add a new user")
+    print("2. Remove a user")
+    print("3. Arm GISS")
+    print("4. Disarm GISS")
+    print("5. Access ADMIN Profile")
+
+    desiredAction = input("Enter number: ")
+
+    if desiredAction == '5':
+        ADMIN_menu()
+    elif desiredAction == '1':
+       # ADMIN_menu()
+    elif desiredAction == '2':
+       # ADMIN_menu()
+    elif desiredAction == '3':
+       # ADMIN_menu()
+    elif desiredAction == '4':
+       # ADMIN_menu()
+
+
+    else:
+        print("ERROR! This function is still being developed. Please try again later!")
+
+def addNewUser():
+
+
+
+ADMIN_menu_SETUP()
+main_menu()
