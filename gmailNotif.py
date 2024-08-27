a25b032b857ac27dc1d148f2f38e92ddfc14de17
ylian121 <165940838+ylian121@users.noreply.github.com>
2024-08-27 14:55:01 -0700

    Update gmailNotif.py

--- gmailNotif.py
+++ gmailNotif.py
@@ -1,7 +1,4 @@
-from new_user import capture_new_user, facial_recognition
-from delete_user import delete_user
-from faceRec import face_recognition
-from peopleDetectionFunction import detect_people
+from facialRecognition import face_recognition
 
 import cv2
 import os
