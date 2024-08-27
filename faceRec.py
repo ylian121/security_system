d4a72ffcfb9634660153b2206c6a499ee83e78ca
Yongyan Liang <yongyanliang@YYL.attlocal.net>
2024-08-26 18:23:32 -0700

    print name in terminal

--- faceRec.py
+++ faceRec.py
@@ -32,6 +32,9 @@ def face_recognition(folder="faces"):
         # Detect Faces
         face_locations, face_names = sfr.detect_known_faces(frame)
         for face_loc, name in zip(face_locations, face_names):
+
+            print(f"Recognized: {name}")
+            
             y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
 
             cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
